f47a5d3e0fec3bfa4b1d8e551490af46d5c7cd36
kennethCarl <kennethcarlybanez@gmail.com>
2022-04-01 22:23:47 +0800

    base code

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') . '?v=' . 'build_' . rand(1, 100) }}" defer></script>
+
+    <!-- Fonts -->
+    {{--<link rel="stylesheet" href="{{asset('font-awesome-4.7.0/css/font-awesome.css')}}">--}}
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') . '?v=' . 'build_' . rand(1, 100) }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" v-cloak>
+        <example-component></example-component>
+    </div>
+</body>
+</html>
